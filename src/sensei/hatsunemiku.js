c64666adde1de176161e80d13ffcac338c882f27
jcote <jordocote@gmail.com>
2023-08-04 12:14:23 -0400

    hatsunemiku sensei

---
+++ src/sensei/hatsunemiku.js
@@ -0,0 +1,94 @@
+'use strict';
+const fs = require('fs');
+const Q = require('q');
+var http = require('http');
+const brain = require('brain.js');
+
+class HatsuneMiku {
+
+    constructor() {
+        this.promises = Q();
+    }
+
+    teach(args) {
+        var self = this;
+        console.log('Starting Hatsune Miku neural network teaching program...');
+
+        // args
+        var maxLines = 100;
+        var maxIterations = 1500;
+        if (args.length < 2) {
+            console.log("Takes 2 arguments: maxLines maxIterations (outputFilename)");
+            console.log("Defaulting to 100 1500 ./corpus/hatsune_miku_lyrics_4k.txt");
+        } else {
+            maxLines = args[0];
+            maxIterations = args[1];    
+        }
+        var outputFilename;
+        if (args.length > 2) {
+            outputFilename = args[2];
+        } else {
+            outputFilename = "/../../data/hatsune-training-data.json"
+        }
+
+        // read
+        console.log('Reading hatsune miku lyrics corpus...');
+        var i = 0;
+        const trainingInputData = [];
+        const allFileContents = fs.readFileSync('./corpus/hatsune_miku_lyrics_4k.txt', 'utf-8');
+        allFileContents.split(/\r?\n/).every(line =>  {
+            i++;
+            console.log(`Line from file: ${line}`);
+            trainingInputData.push(line);
+            if (i >= maxLines) {
+                return false;
+            }
+            return true;
+        });
+        const used = process.memoryUsage().heapUsed / 1024 / 1024;
+        console.log(`The list uses approximately ${Math.round(used * 100) / 100} MB`);
+        console.log(`The list is ${trainingInputData.length} lines long`);
+
+        // train
+        const lstm = new brain.recurrent.LSTM();
+        const result = lstm.train(trainingInputData, {
+            iterations: maxIterations,
+            log: details => console.log(details),
+            errorThresh: 0.011
+        });
+
+        // store
+        const trainingOutputData = lstm.toJSON();
+
+        // create training data fragment
+        promises = promises.then(() => {
+            console.log('---HatsuneMikuTrainingDataLyrics---');
+            var data = {
+                    name: 'HatsuneMikuTrainingDataLyrics',
+                    astid: null, 
+                    fn: trainingOutputData, 
+                    fntype: 'object',
+                    argnum: 0, 
+                    argtypes: [], 
+                    modules: [], 
+                    memoize: false, 
+                    promise: false
+            };
+
+            return self.apiClient.createStoredFunction(data);
+        });
+
+
+
+
+        self.promises = self.promises.then(new Promise((resolve) => {console.log('Completed teaching hatsune miku neural network training setup.'); resolve()}));
+
+        self.promises = self.promises.then(this.teachSub());
+
+        
+        return self.promises;
+    }
+}
+
+
+exports.HatsuneMiku = HatsuneMiku;
